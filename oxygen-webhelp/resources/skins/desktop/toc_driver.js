86873a71d94e5246a9818d72388ced8fc9b84274
Peter Logan <pml1951@users.noreply.github.com>
2018-01-14 17:56:01 -0500

    change skin
    also update regex

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20171228083512", "oxygen-webhelp/search/index-1.js?uniqueId=20171228083512", "oxygen-webhelp/search/index-2.js?uniqueId=20171228083512", "oxygen-webhelp/search/index-3.js?uniqueId=20171228083512"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180114055013", "oxygen-webhelp/search/index-1.js?uniqueId=20180114055013", "oxygen-webhelp/search/index-2.js?uniqueId=20180114055013", "oxygen-webhelp/search/index-3.js?uniqueId=20180114055013"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
