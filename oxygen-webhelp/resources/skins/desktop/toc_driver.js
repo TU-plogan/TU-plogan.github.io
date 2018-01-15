ba095e968ee47856a89e91416600b209b9c8eb14
Peter Logan <pml1951@users.noreply.github.com>
2018-01-14 21:02:20 -0500

    tweak

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180114084823", "oxygen-webhelp/search/index-1.js?uniqueId=20180114084823", "oxygen-webhelp/search/index-2.js?uniqueId=20180114084823", "oxygen-webhelp/search/index-3.js?uniqueId=20180114084823"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180114085916", "oxygen-webhelp/search/index-1.js?uniqueId=20180114085916", "oxygen-webhelp/search/index-2.js?uniqueId=20180114085916", "oxygen-webhelp/search/index-3.js?uniqueId=20180114085916"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
