c42d20197458373c1fc7f7410e6cac32840c2f08
Peter Logan <pml1951@users.noreply.github.com>
2018-01-20 23:22:06 -0500

    Break up topics to smaller modules
    change general topics to concepts, tasks, or reference types

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180119094145", "oxygen-webhelp/search/index-1.js?uniqueId=20180119094145", "oxygen-webhelp/search/index-2.js?uniqueId=20180119094145", "oxygen-webhelp/search/index-3.js?uniqueId=20180119094145"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180120111651", "oxygen-webhelp/search/index-1.js?uniqueId=20180120111651", "oxygen-webhelp/search/index-2.js?uniqueId=20180120111651", "oxygen-webhelp/search/index-3.js?uniqueId=20180120111651"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
