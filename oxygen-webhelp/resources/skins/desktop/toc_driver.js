78db28a99e339185f6bc01f6cc9d359d66921be1
Peter Logan <pml1951@users.noreply.github.com>
2018-02-26 22:39:47 -0500

    add file/folder names page

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180226082341", "oxygen-webhelp/search/index-1.js?uniqueId=20180226082341", "oxygen-webhelp/search/index-2.js?uniqueId=20180226082341", "oxygen-webhelp/search/index-3.js?uniqueId=20180226082341"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180226100557", "oxygen-webhelp/search/index-1.js?uniqueId=20180226100557", "oxygen-webhelp/search/index-2.js?uniqueId=20180226100557", "oxygen-webhelp/search/index-3.js?uniqueId=20180226100557"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
