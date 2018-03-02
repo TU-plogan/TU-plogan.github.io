776eb16d3afff032829c8c6717a014530745cbcc
Peter Logan <pml1951@users.noreply.github.com>
2018-03-01 22:07:34 -0500

    update ids, add footnote page

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180228092759", "oxygen-webhelp/search/index-1.js?uniqueId=20180228092759", "oxygen-webhelp/search/index-2.js?uniqueId=20180228092759", "oxygen-webhelp/search/index-3.js?uniqueId=20180228092759"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180301100234", "oxygen-webhelp/search/index-1.js?uniqueId=20180301100234", "oxygen-webhelp/search/index-2.js?uniqueId=20180301100234", "oxygen-webhelp/search/index-3.js?uniqueId=20180301100234"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
