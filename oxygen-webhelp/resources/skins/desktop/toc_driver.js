4b8563c67b150af329f81e1a4f32fded93745b05
Peter Logan <pml1951@users.noreply.github.com>
2018-03-07 09:50:26 -0500

    add tei standards page
    reorganize reference section

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180302113159", "oxygen-webhelp/search/index-1.js?uniqueId=20180302113159", "oxygen-webhelp/search/index-2.js?uniqueId=20180302113159", "oxygen-webhelp/search/index-3.js?uniqueId=20180302113159"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180306094622", "oxygen-webhelp/search/index-1.js?uniqueId=20180306094622", "oxygen-webhelp/search/index-2.js?uniqueId=20180306094622", "oxygen-webhelp/search/index-3.js?uniqueId=20180306094622"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
