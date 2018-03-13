7ab393c9f662d2c503c191bc72276ed683b35297
Peter Logan <pml1951@users.noreply.github.com>
2018-03-13 16:16:54 -0400

    changed OCR-training page

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180313104627", "oxygen-webhelp/search/index-1.js?uniqueId=20180313104627", "oxygen-webhelp/search/index-2.js?uniqueId=20180313104627", "oxygen-webhelp/search/index-3.js?uniqueId=20180313104627"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180313041334", "oxygen-webhelp/search/index-1.js?uniqueId=20180313041334", "oxygen-webhelp/search/index-2.js?uniqueId=20180313041334", "oxygen-webhelp/search/index-3.js?uniqueId=20180313041334"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
