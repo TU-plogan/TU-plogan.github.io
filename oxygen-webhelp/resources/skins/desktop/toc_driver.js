04f4f555de571a520042530856e9bbb92f125dce
Peter Logan <pml1951@users.noreply.github.com>
2018-04-20 22:39:27 -0400

    Add "about" page

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180330033024", "oxygen-webhelp/search/index-1.js?uniqueId=20180330033024", "oxygen-webhelp/search/index-2.js?uniqueId=20180330033024", "oxygen-webhelp/search/index-3.js?uniqueId=20180330033024"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180420103741", "oxygen-webhelp/search/index-1.js?uniqueId=20180420103741", "oxygen-webhelp/search/index-2.js?uniqueId=20180420103741", "oxygen-webhelp/search/index-3.js?uniqueId=20180420103741"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
