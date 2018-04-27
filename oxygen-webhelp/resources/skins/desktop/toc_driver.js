7f5ea2320f43e72183dae40dc369847e3f883fd9
Peter Logan <pml1951@users.noreply.github.com>
2018-04-27 18:06:05 -0400

    Add page on image resolution

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180424095526", "oxygen-webhelp/search/index-1.js?uniqueId=20180424095526", "oxygen-webhelp/search/index-2.js?uniqueId=20180424095526", "oxygen-webhelp/search/index-3.js?uniqueId=20180424095526"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180427060123", "oxygen-webhelp/search/index-1.js?uniqueId=20180427060123", "oxygen-webhelp/search/index-2.js?uniqueId=20180427060123", "oxygen-webhelp/search/index-3.js?uniqueId=20180427060123"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
