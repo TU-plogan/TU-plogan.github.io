f822c5129f83d390c42a7050cf6e4b79aa81b75a
Peter Logan <pml1951@users.noreply.github.com>
2018-05-08 22:56:00 -0400

    update unicode

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180507061844", "oxygen-webhelp/search/index-1.js?uniqueId=20180507061844", "oxygen-webhelp/search/index-2.js?uniqueId=20180507061844", "oxygen-webhelp/search/index-3.js?uniqueId=20180507061844"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180508105319", "oxygen-webhelp/search/index-1.js?uniqueId=20180508105319", "oxygen-webhelp/search/index-2.js?uniqueId=20180508105319", "oxygen-webhelp/search/index-3.js?uniqueId=20180508105319"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
