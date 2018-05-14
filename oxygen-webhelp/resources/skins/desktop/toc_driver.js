b309485454b50ad99054106bc7ca3973a30f0f44
Peter Logan <pml1951@users.noreply.github.com>
2018-05-14 13:09:16 -0400

    update pages

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180511103648", "oxygen-webhelp/search/index-1.js?uniqueId=20180511103648", "oxygen-webhelp/search/index-2.js?uniqueId=20180511103648", "oxygen-webhelp/search/index-3.js?uniqueId=20180511103648"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180514010514", "oxygen-webhelp/search/index-1.js?uniqueId=20180514010514", "oxygen-webhelp/search/index-2.js?uniqueId=20180514010514", "oxygen-webhelp/search/index-3.js?uniqueId=20180514010514"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
