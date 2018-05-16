d3975b757249d92837ad908b5ff8daec919daa94
Peter Logan <pml1951@users.noreply.github.com>
2018-05-16 12:02:44 -0400

    update folder structure

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180514051324", "oxygen-webhelp/search/index-1.js?uniqueId=20180514051324", "oxygen-webhelp/search/index-2.js?uniqueId=20180514051324", "oxygen-webhelp/search/index-3.js?uniqueId=20180514051324"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180516115310", "oxygen-webhelp/search/index-1.js?uniqueId=20180516115310", "oxygen-webhelp/search/index-2.js?uniqueId=20180516115310", "oxygen-webhelp/search/index-3.js?uniqueId=20180516115310"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
