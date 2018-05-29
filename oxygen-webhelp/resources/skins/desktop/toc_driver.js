acdb71e36603ebd61f8b9d2db81ec6b6a034b6a7
Peter Logan <pml1951@users.noreply.github.com>
2018-05-29 17:02:39 -0400

    add unicode

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180526081808", "oxygen-webhelp/search/index-1.js?uniqueId=20180526081808", "oxygen-webhelp/search/index-2.js?uniqueId=20180526081808", "oxygen-webhelp/search/index-3.js?uniqueId=20180526081808"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180529045839", "oxygen-webhelp/search/index-1.js?uniqueId=20180529045839", "oxygen-webhelp/search/index-2.js?uniqueId=20180529045839", "oxygen-webhelp/search/index-3.js?uniqueId=20180529045839"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
