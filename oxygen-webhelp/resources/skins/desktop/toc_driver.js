9080ab34e948ab42129356acc47557d280c2cc59
pml1951 <comid@verizon.net>
2018-08-11 21:16:24 -0400

    update pages on checking html code

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180809104239", "oxygen-webhelp/search/index-1.js?uniqueId=20180809104239", "oxygen-webhelp/search/index-2.js?uniqueId=20180809104239", "oxygen-webhelp/search/index-3.js?uniqueId=20180809104239"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180811091442", "oxygen-webhelp/search/index-1.js?uniqueId=20180811091442", "oxygen-webhelp/search/index-2.js?uniqueId=20180811091442", "oxygen-webhelp/search/index-3.js?uniqueId=20180811091442"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
