056a2c62470e41c9e75e3e69a843638d740f05d9
pml1951 <comid@verizon.net>
2018-11-03 23:29:28 -0400

    update page descriptions

---
+++ oxygen-webhelp/app/nav-links/json/t.table-areas-d66e1228.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Editing tables","shortdesc":"\n                  <p class=\"shortdesc\">Table columns and rows can be adjusted before recognition.</p>\n               ","href":"source\/t_edit_tables.html","attributes": {"data-id":"untitled3",},"menu": {"hasChildren":false,},"tocID":"untitled3-d66e1248","topics":[]}]});
