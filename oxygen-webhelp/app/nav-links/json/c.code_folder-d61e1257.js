5c163a8e658d2e0336626677a8dfed3879bf6969
Peter Logan <pml1951@users.noreply.github.com>
2020-02-26 22:03:19 -0500

    update for new repos

---
+++ oxygen-webhelp/app/nav-links/json/c.code_folder-d61e1257.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Python Folder","shortdesc":"<p class=\"shortdesc\">Python script files.</p>","href":"source\/c_python_folder.html","attributes": {"data-id":"c.python-fdr",},"menu": {"hasChildren":false,},"tocID":"c.python-fdr-d61e1285","topics":[]}]});
