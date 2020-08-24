2447180ea3dacfabd1439b7a765ee1894a362187
Peter Logan <pml1951@users.noreply.github.com>
2020-08-24 13:37:01 -0400

    Update information on eb07 OCR projects

---
+++ oxygen-webhelp/app/nav-links/json/t.save-and-output-d61e4780.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Cleanup AFR Output filenames","shortdesc":"<p class=\"shortdesc\">Explains how to change auto-generated filenames created by AFR. </p>","href":"source\/t_cleanup_afr_output.html","attributes": {"data-id":"cleanup_afr_output",},"menu": {"hasChildren":false,},"tocID":"cleanup_afr_output-d61e4823","topics":[]}]});
