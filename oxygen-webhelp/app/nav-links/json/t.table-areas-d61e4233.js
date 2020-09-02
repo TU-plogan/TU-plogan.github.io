69a13220d804340e3aae1ab906e38e428b9112b2
Peter Logan <pml1951@users.noreply.github.com>
2020-09-01 22:10:21 -0400

    update lists, marginal notes pages

---
+++ oxygen-webhelp/app/nav-links/json/t.table-areas-d61e4233.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Edit tables","shortdesc":"<p class=\"shortdesc\">Table columns and rows can be adjusted before recognition.</p>","href":"source\/t_edit_tables.html","attributes": {"data-id":"edit_tables",},"menu": {"hasChildren":false,},"tocID":"edit_tables-d61e4278","topics":[]}]});
