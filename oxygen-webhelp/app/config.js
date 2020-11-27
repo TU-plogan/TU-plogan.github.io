34baf6f851b452030616dc0197fe8e10b535380f
Peter Logan <pml1951@users.noreply.github.com>
2020-11-27 17:20:39 -0500

    update folder names

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20201126173401';
+                args = '20201127171757';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
