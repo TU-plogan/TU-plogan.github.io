452d11c62ef3924dace86d97477c22bf55ff447b
Peter Logan <pml1951@users.noreply.github.com>
2021-02-11 17:51:24 -0500

    fix one regex

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210209212944';
+                args = '20210211174907';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
