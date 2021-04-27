4b362028578ae960f2846a726a6791ce7110bb5c
Peter Logan <pml1951@users.noreply.github.com>
2021-04-26 21:26:32 -0400

    update cleanup procededures

---
+++ oxygen-webhelp/app/nav-links/json/t.create-ocr-project-d1150e5221.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"Image Preprocessing","shortdesc":"<p class=\"shortdesc\">Preprocess all images in a new OCR Project before drawing boxes.<\/p>","href":"source\/t_image_preprocessing.html","attributes":{"data-id":"image_preprocessing"},"menu":{"hasChildren":false},"tocID":"image_preprocessing-d1150e5286","topics":[]}]});
