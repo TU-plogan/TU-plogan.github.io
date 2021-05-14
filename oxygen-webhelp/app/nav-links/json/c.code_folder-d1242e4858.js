b96a7b600376a13be0fba5b0e55becd6cb5e8124
Peter Logan <pml1951@users.noreply.github.com>
2021-05-13 22:18:32 -0400

    add new section on creating master files

---
+++ oxygen-webhelp/app/nav-links/json/c.code_folder-d1242e4858.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"<span class=\"ph filepath\">Python<\/span> Folder","shortdesc":"<p class=\"shortdesc\">Python script files.<\/p>","href":"source\/c_python_folder.html","attributes":{"data-id":"c.python-fdr"},"menu":{"hasChildren":false},"tocID":"c.python-fdr-d1242e4933","topics":[]}]});
