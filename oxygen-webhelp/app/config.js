52bf5f2e7bda007705df0a122b79ec70986ce3ab
Peter Logan <pml1951@users.noreply.github.com>
2021-05-17 12:30:22 -0400

    update introduction

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210513221558';
+                args = '20210517122704';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
