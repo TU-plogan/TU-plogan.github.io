1f3219b34b1d25ec3bc061ffbee9c1ed27f451b2
Peter Logan <pml1951@users.noreply.github.com>
2021-05-19 18:34:36 -0400

    add <byline> to TEI stylesheet

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210517122704';
+                args = '20210519183340';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
