8ddf307e86f0356ced849b11465445c9a324ef4c
Peter Logan <pml1951@users.noreply.github.com>
2021-08-01 14:27:55 -0400

    add page for checking @-codes with Python

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210723203643';
+                args = '20210801142610';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
