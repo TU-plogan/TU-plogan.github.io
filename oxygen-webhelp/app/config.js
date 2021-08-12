8c41bd571bfc84d5271c7d8d2419c8c3fb50045f
Peter Logan <pml1951@users.noreply.github.com>
2021-08-11 20:51:39 -0400

    update style manual

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210805143910';
+                args = '20210811205012';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
