2bca583c102449bf775fcace124b7d9f49088c3c
Peter Logan <pml1951@users.noreply.github.com>
2021-10-28 17:29:02 -0400

    fix clean-up page

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20211028170523';
+                args = '20211028172801';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
