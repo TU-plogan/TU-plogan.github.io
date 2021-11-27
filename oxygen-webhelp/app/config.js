a1e0cc494af88a2b6c02fb9a6ed3eb25df59e3c1
Peter Logan <pml1951@users.noreply.github.com>
2021-11-27 13:20:23 -0500

    Update "cleanup page files" regex's

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -110,7 +110,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20211028173636';
+                args = '20211127131755';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
